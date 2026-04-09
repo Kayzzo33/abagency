1b38347b88dd0e749c7050b2aaae4ff4eba48554
xKayky <gcalternativeai@gmail.com>
2026-04-09 18:31:29 -0300

    feat: add predefined message to WhatsApp links

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ import "./styles/Contact.css";
 
 const Contact = () => {
   const JKAYZZ_WA = "https://wa.me/5573988027104?text=Vi%20o%20seu%20portf%C3%B3lio%20da%20AB%20Agency%20e%20me%20interessei%20em%20saber%20como%20posso%20estar%20desenvolvendo%20o%20meu%20site%20com%20voc%C3%AA.";
+  const AB_AGENCY_WA = "https://wa.me/5599992055510?text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20AB%20Agency%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es.";
 
   return (
     <div className="contact-section section-container" id="contact">
@@ -18,7 +19,7 @@ const Contact = () => {
             </p>
             <h4>Telefone / WhatsApp</h4>
             <p>
-              <a href="https://wa.me/5599992055510" target="_blank" data-cursor="disable">
+              <a href={AB_AGENCY_WA} target="_blank" data-cursor="disable">
                 +55 99 9 9205-5510
               </a>
             </p>
@@ -30,7 +31,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://wa.me/5599992055510"
+              href={AB_AGENCY_WA}
               target="_blank"
               data-cursor="disable"
               className="contact-social"
